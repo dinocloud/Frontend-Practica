23159e9b325b9b21bf1871b3faa15c77c4bd0c00
Rodrigo Ovalle <rodrigo.ovalle@dinocloudconsulting.com>
2017-07-25 12:20:20 -0300

    Add moment

--- src/providers/user-tasks/user-tasks.ts
+++ src/providers/user-tasks/user-tasks.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the UserTasksProvider provider.
+import * as moment from 'moment';
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular DI.
-*/
+import {Task} from "../../models/task";
+
+
 @Injectable()
 export class UserTasksProvider {
 
   constructor() {
-    console.log('Hello UserTasksProvider Provider');
+  }
+
+  public getTasks(userId : number) : Array<Task> {
+    let userTasks : Array<Task> = new Array<Task>();
+
+    userTasks.push(new Task(0, 'Task #1', 'asdfg0',
+                  moment().toISOString(), 'DONE', [], userId));
+    userTasks.push(new Task(1, 'Task #2', 'asdfg1',
+                  moment().subtract(14, 'hour').toISOString(), 'DONE', [], userId));
+    userTasks.push(new Task(2, 'Task #3', 'asdfg2',
+                  moment().add(1, 'week').toISOString(), 'DONE', [], userId));
+
+    return userTasks;
+
   }
 
 }
